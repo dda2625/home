3bbfc486bb4bc9bfe82286711b81a1812e464b39
Simon Bjerre <bjerrecs@protonmail.com>
2024-03-05 23:13:10 +0100

    w

--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -1,8 +1,16 @@
 /** @type {import('tailwindcss').Config} */
+
 export default {
 	content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
 	theme: {
-		extend: {},
+		extend: {
+			colors: {
+				'vatsca1': '#43c6e7',
+				'vatsca2': '#1a475f',
+				'vatsca3': '#011328',
+				'snow': '#dfebeb',
+			}
+		}
 	},
 	plugins: [],
 }
