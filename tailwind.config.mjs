ce44fce4354cd493d1f717aee7ad2ccbacb4cb2f
Simon Bjerre <simon.bjerre@team.blue>
2024-03-09 11:06:48 +0100

    added skeleton for bookings

--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -1,7 +1,8 @@
 /** @type {import('tailwindcss').Config} */
+const { nextui } = require("@nextui-org/react");
 
 export default {
-	content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
+	content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}',"./node_modules/@nextui-org/theme/dist/**/*.{js,ts,jsx,tsx}"],
 	theme: {
 		extend: {
 			colors: {
@@ -9,8 +10,11 @@ export default {
 				'vatsca2': '#1a475f',
 				'vatsca3': '#011328',
 				'snow': '#dfebeb',
+				'success': '#41826e',
+				'danger': '#b63f3f'
 			}
 		}
 	},
-	plugins: [],
+	darkMode: "class",
+  	plugins: [nextui()]
 }
