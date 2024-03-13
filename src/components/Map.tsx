a78826158a0d21ac198a3f5c6676a9b45e6242a2
Simon Bjerre <bjerrecs@protonmail.com>
2024-03-13 18:40:21 +0100

    dark mode

--- src/components/Map.tsx
+++ src/components/Map.tsx
@@ -2,7 +2,6 @@ export default function Map() {
   return (
     <div className="w-full h-full flex items-center justify-center">
         <img src="/map_black.png" className="h-[75vh]"/>
-
     </div>
   )
 }
