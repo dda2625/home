a46179e1a941188021d275fa364be8697cfe069b
Simon Bjerre <simon.bjerre@team.blue>
2024-03-16 20:40:27 +0100

    added about page and updated index

---
+++ src/components/Breadcrumbs.tsx
@@ -0,0 +1,15 @@
+import React from "react";
+import {Breadcrumbs, BreadcrumbItem} from "@nextui-org/react";
+
+export default function App() {
+
+  return (
+    <div className="grid col-span-4  flex-col flex-wrap gap-4 w-full justify-center items-center">
+        <Breadcrumbs size="lg">
+          <BreadcrumbItem><a href="/">Home</a></BreadcrumbItem>
+          <BreadcrumbItem><a href="/pilots">Pilots</a></BreadcrumbItem>
+          <BreadcrumbItem><a href="/airports-and-charts/denmark">Denmark</a></BreadcrumbItem>
+        </Breadcrumbs>
+    </div>
+  );
+}
