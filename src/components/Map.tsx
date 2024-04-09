61b91d22045dfd1604519cd68defa11e8278e91c
Simon Bjerre <bjerrecs@protonmail.com>
2024-04-09 13:24:50 +0200

    updated backgroud

--- src/components/Map.tsx
+++ src/components/Map.tsx
@@ -1,7 +1,7 @@
 export default function Map() {
   return (
     <div className="w-full h-full flex items-center justify-center">
-        <img src="/scandinavia.svg" className="w-5/6"/>
+        <img src="https://cc.vatsim-scandinavia.org/images/undraw_speech_to_text_vatsim.svg" className="w-5/6"/>
     </div>
   )
 }
