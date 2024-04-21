e7443dab7e9450a8897776c604006422ec1867ba
Simon Bjerre <bjerrecs@protonmail.com>
2024-04-21 11:30:25 +0200

    wokring demo

--- src/components/BookingsComponent.tsx
+++ src/components/BookingsComponent.tsx
@@ -1,376 +1,137 @@
-import convertZulu from "../utils/convertZulu";
 import bookingType from "../utils/bookingType";
+import convertZulu from "../utils/convertZulu";
 
-var mockCCData = {
-    data: [
-      {
-        id: 9,
-        source: "CC",
-        vatsim_booking: null,
-        callsign: "EKDK_B_CTR",
-        position_id: 19,
-        name: "Web Team",
-        time_start: "2024-04-10 12:00:00",
-        time_end: "2020-12-09 14:00:00",
-        user_id: 10000010,
-        training: 0,
-        event: 0,
-        exam: 0,
-        deleted: 0,
-        created_at: "2024-03-25T20:24:00.000000Z",
-        updated_at: "2020-12-03T20:24:00.000000Z",
-      },
-      {
-        id: 4,
-        source: "CC",
-        vatsim_booking: null,
-        callsign: "EKAH_TWR",
-        position_id: 2,
-        name: "Web Team",
-        time_start: "2024-04-10 12:00:00",
-        time_end: "2020-12-10 23:00:00",
-        user_id: 10000010,
-        training: 0,
-        event: 0,
-        exam: 1,
-        deleted: 0,
-        created_at: "2020-12-03T19:55:34.000000Z",
-        updated_at: "2020-12-03T19:55:34.000000Z",
-      },
-      {
-        id: 8,
-        source: "CC",
-        vatsim_booking: null,
-        callsign: "EFHA_TWR",
-        position_id: 59,
-        name: "Web Two",
-        time_start: "2024-04-10 12:00:00",
-        time_end: "2020-12-10 12:00:00",
-        user_id: 10000002,
-        training: 1,
-        event: 0,
-        exam: 0,
-        deleted: 0,
-        created_at: "2020-12-03T20:00:25.000000Z",
-        updated_at: "2020-12-03T20:00:25.000000Z",
-      },
-      {
-        id: 10,
-        source: "CC",
-        vatsim_booking: null,
-        callsign: "EKBI_APP",
-        position_id: 3,
-        name: "Web Team",
-        time_start: "2024-04-10 11:11:00",
-        time_end: "2020-12-16 12:12:00",
-        user_id: 10000010,
-        training: 0,
-        event: 0,
-        exam: 1,
-        deleted: 0,
-        created_at: "2020-12-04T15:18:08.000000Z",
-        updated_at: "2020-12-04T15:18:08.000000Z",
-      },
-      {
-        id: 1,
-        source: "CC",
-        vatsim_booking: null,
-        callsign: "ENGK_I_TWR",
-        position_id: 191,
-        name: "Web Team",
-        time_start: "2024-04-10 21:00:00",
-        time_end: "2020-12-17 22:00:00",
-        user_id: 10000010,
-        training: 0,
-        event: 0,
-        exam: 0,
-        deleted: 0,
-        created_at: "2020-12-03T19:55:04.000000Z",
-        updated_at: "2020-12-03T19:55:39.000000Z",
-      },
-      {
-        id: 7,
-        source: "CC",
-        vatsim_booking: null,
-        callsign: "EKAH_TWR",
-        position_id: 2,
-        name: "Web Six",
-        time_start: "2020-12-24 12:00:00",
-        time_end: "2020-12-24 15:00:00",
-        user_id: 10000006,
-        training: 0,
-        event: 0,
-        exam: 0,
-        deleted: 0,
-        created_at: "2020-12-03T19:58:05.000000Z",
-        updated_at: "2020-12-03T19:58:05.000000Z",
-      },
-      {
-        id: 6,
-        source: "CC",
-        vatsim_booking: null,
-        callsign: "ENFL_I_TWR",
-        position_id: 190,
-        name: "Web Six",
-        time_start: "2020-12-29 12:00:00",
-        time_end: "2020-12-29 13:00:00",
-        user_id: 10000006,
-        training: 0,
-        event: 0,
-        exam: 0,
-        deleted: 0,
-        created_at: "2020-12-03T19:57:05.000000Z",
-        updated_at: "2020-12-03T19:57:05.000000Z",
-      },
-      {
-        id: 3,
-        source: "CC",
-        vatsim_booking: null,
-        callsign: "ESDF_TWR",
-        position_id: 280,
-        name: "Web Team",
-        time_start: "2020-12-30 12:00:00",
-        time_end: "2020-12-30 12:00:00",
-        user_id: 10000010,
-        training: 0,
-        event: 1,
-        exam: 0,
-        deleted: 0,
-        created_at: "2020-12-03T19:55:26.000000Z",
-        updated_at: "2020-12-03T19:55:26.000000Z",
-      },
-      {
-        id: 2,
-        source: "CC",
-        vatsim_booking: null,
-        callsign: "BIRD_1_CTR",
-        position_id: 111,
-        name: "Web Team",
-        time_start: "2020-12-31 12:12:00",
-        time_end: "2020-12-31 15:00:00",
-        user_id: 10000010,
-        training: 1,
-        event: 0,
-        exam: 0,
-        deleted: 0,
-        created_at: "2020-12-03T19:55:13.000000Z",
-        updated_at: "2020-12-03T19:55:13.000000Z",
-      },
-      {
-        id: 12,
-        source: "CC",
-        vatsim_booking: null,
-        callsign: "EKAH_APP",
-        position_id: 1,
-        name: "Web Six",
-        time_start: "2021-01-29 12:00:00",
-        time_end: "2021-01-29 13:00:00",
-        user_id: 10000006,
-        training: 0,
-        event: 0,
-        exam: 0,
-        deleted: 0,
-        created_at: "2021-01-28T18:57:52.000000Z",
-        updated_at: "2021-01-28T18:57:52.000000Z",
-      },
-      {
-        id: 11,
-        source: "CC",
-        vatsim_booking: null,
-        callsign: "EKAH_TWR",
-        position_id: 2,
-        name: "Web Six",
-        time_start: "2021-01-30 12:00:00",
-        time_end: "2021-01-30 12:00:00",
-        user_id: 10000006,
-        training: 0,
-        event: 0,
-        exam: 0,
-        deleted: 0,
-        created_at: "2021-01-28T18:57:24.000000Z",
-        updated_at: "2021-01-28T18:57:24.000000Z",
-      },
-      {
-        id: 14,
-        source: "CC",
-        vatsim_booking: null,
-        callsign: "EKAH_TWR",
-        position_id: 2,
-        name: "Web Team",
-        time_start: "2021-03-10 12:00:00",
-        time_end: "2021-03-10 14:00:00",
-        user_id: 10000010,
-        training: 1,
-        event: 0,
-        exam: 0,
-        deleted: 0,
-        created_at: "2021-03-04T20:40:19.000000Z",
-        updated_at: "2021-03-04T20:40:19.000000Z",
-      },
-      {
-        id: 15,
-        source: "CC",
-        vatsim_booking: null,
-        callsign: "EKAH_APP",
-        position_id: 1,
-        name: "Web Ten",
-        time_start: "2022-01-10 12:00:00",
-        time_end: "2022-01-10 15:00:00",
-        user_id: 10000010,
-        training: 0,
-        event: 0,
-        exam: 0,
-        deleted: 0,
-        created_at: "2022-01-02T14:36:24.000000Z",
-        updated_at: "2022-01-02T14:36:24.000000Z",
-      },
-      {
-        id: 17,
-        source: "CC",
-        vatsim_booking: null,
-        callsign: "ENGM_E_TWR",
-        position_id: 200,
-        name: "Web Ten",
-        time_start: "2022-05-21 12:00:00",
-        time_end: "2022-05-21 13:00:00",
-        user_id: 10000010,
-        training: 0,
-        event: 0,
-        exam: 1,
-        deleted: 0,
-        created_at: "2022-05-19T17:41:41.000000Z",
-        updated_at: "2022-05-19T17:41:41.000000Z",
-      },
-      {
-        id: 18,
-        source: "CC",
-        vatsim_booking: null,
-        callsign: "EKCH_GND",
-        position_id: 11,
-        name: "Web Ten",
-        time_start: "2022-05-21 12:00:00",
-        time_end: "2022-05-21 13:00:00",
-        user_id: 10000010,
-        training: 0,
-        event: 0,
-        exam: 1,
-        deleted: 0,
-        created_at: "2022-05-19T17:41:41.000000Z",
-        updated_at: "2022-05-19T17:41:41.000000Z",
-      },
-      {
-        id: 19,
-        source: "CC",
-        vatsim_booking: null,
-        callsign: "ENBR_TWR",
-        position_id: 178,
-        name: "Web Ten",
-        time_start: "2023-03-15 12:00:00",
-        time_end: "2023-03-15 14:00:00",
-        user_id: 10000010,
-        training: 0,
-        event: 0,
-        exam: 0,
-        deleted: 0,
-        created_at: "2023-03-04T17:36:25.000000Z",
-        updated_at: "2023-03-04T17:36:25.000000Z",
-      },
-      {
-        id: 20,
-        source: "CC",
-        vatsim_booking: null,
-        callsign: "EFHA_APP",
-        position_id: 58,
-        name: "Web Ten",
-        time_start: "2023-03-16 12:00:00",
-        time_end: "2023-03-16 13:00:00",
-        user_id: 10000010,
-        training: 0,
-        event: 0,
-        exam: 0,
-        deleted: 0,
-        created_at: "2023-03-04T17:37:45.000000Z",
-        updated_at: "2023-03-04T17:37:45.000000Z",
-      },
-      {
-        id: 21,
-        source: "CC",
-        vatsim_booking: null,
-        callsign: "EFET_I_TWR",
-        position_id: 57,
-        name: "Web Two",
-        time_start: "2023-03-16 12:13:00",
-        time_end: "2023-03-16 14:15:00",
-        user_id: 10000002,
-        training: 1,
-        event: 0,
-        exam: 0,
-        deleted: 0,
-        created_at: "2023-03-04T17:54:22.000000Z",
-        updated_at: "2023-03-04T17:54:22.000000Z",
-      },
-      {
-        id: 22,
-        source: "CC",
-        vatsim_booking: null,
-        callsign: "EKBI_APP",
-        position_id: 3,
-        name: "Web Ten",
-        time_start: "2023-08-24 12:00:00",
-        time_end: "2023-08-24 13:00:00",
-        user_id: 10000010,
-        training: 0,
-        event: 0,
-        exam: 0,
-        deleted: 0,
-        created_at: "2023-08-23T18:03:33.000000Z",
-        updated_at: "2023-08-23T18:03:33.000000Z",
-      },
-    ],
-  };
+var ControlCenterBookingInformation = {
+  data: [
+    {
+      id: 23,
+      callsign: "EKDK_CTR",
+      time_start: "2024-04-21 20:00:00",
+      time_end: "2024-04-21 21:00:00",
+      training: 0,
+      event: 1,
+      exam: 0,
+      created_at: "2024-04-13T19:28:25.000000Z",
+      updated_at: "2024-04-13T19:28:25.000000Z",
+    },
+    {
+      id: 23,
+      callsign: "EKDK_B_CTR",
+      time_start: "2024-04-21 20:00:00",
+      time_end: "2024-04-21 21:00:00",
+      training: 0,
+      event: 1,
+      exam: 0,
+      created_at: "2024-04-13T19:28:25.000000Z",
+      updated_at: "2024-04-13T19:28:25.000000Z",
+    },
+    {
+      id: 23,
+      callsign: "EKDK_V_CTR",
+      time_start: "2024-04-21 19:00:00",
+      time_end: "2024-04-21 21:00:00",
+      training: 0,
+      event: 0,
+      exam: 1,
+      created_at: "2024-04-13T19:28:25.000000Z",
+      updated_at: "2024-04-13T19:28:25.000000Z",
+    },
+    {
+      id: 23,
+      callsign: "EKDK_D_CTR",
+      time_start: "2024-04-22 20:00:00",
+      time_end: "2024-04-22 21:00:00",
+      training: 1,
+      event: 0,
+      exam: 0,
+      created_at: "2024-04-13T19:28:25.000000Z",
+      updated_at: "2024-04-13T19:28:25.000000Z",
+    },
+  ],
+};
 
-//  fetch('http://cc-test.vatsca.org/api/open/bookings')
-//  .then(response => response.json())
-//  .then(data => mockCCData = data)
-//  .catch(error => console.error(error));
+function sortBookingsByStartTime(bookings: any) {
+  return bookings.sort((a, b) => new Date(a.time_start) - new Date(b.time_start));
+}
 
+const sortedBookings = sortBookingsByStartTime(ControlCenterBookingInformation.data);
+
+//fetch('http://cc-test.vatsca.org/api/open/bookings')
+//.then(response => response.json())
+//.then(data => ControlCenterBookingInformation = data)
+//.catch(error => console.error(error));
+
+var VatsimDataFeed = {
+  controllers: [
+    {
+      cid: 1385367,
+      name: "1385367",
+      callsign: "EKDK_CTR",
+      frequency: "136.485",
+      facility: 1,
+      rating: 5,
+      server: "CANADA",
+      visual_range: 1500,
+      text_atis: ["ATC by New York Center HF relay by New York ARINC"],
+      last_updated: "2024-04-21T19:57:59.1843559Z",
+      logon_time: "2024-04-21T14:47:28.6331726Z",
+    },
+  ],
+};
+
+//fetch('https://data.vatsim.net/v3/vatsim-data.json')
+//.then(response => response.json())
+//.then(data => VatsimDataFeed = data)
+//.catch(error => console.error(error));
+
+function isOnline(CALLSIGN: string) {
+  var online = false;
+  VatsimDataFeed.controllers.forEach((controller) => {
+    if (controller.callsign === CALLSIGN) {
+      online = true;
+    }
+  });
+  return online;
+}
+
+const feed = ControlCenterBookingInformation.data
 
 // Helper function to group bookings by day
 function groupByDay(bookings: any) {
-    return bookings.reduce((groupedBookings: { [x: string]: any[]; }, booking: { time_start: string | number | Date; }) => {
-        const date = new Date(booking.time_start).toDateString();
-        if (!groupedBookings[date]) {
-            groupedBookings[date] = [];
-        }
-        groupedBookings[date].push(booking);
-        return groupedBookings;
-    }, {});
+  return bookings.reduce((groupedBookings: { [x: string]: any[]; }, booking: { time_start: string | number | Date; }) => {
+      const date = new Date(booking.time_start).toDateString();
+      if (!groupedBookings[date]) {
+          groupedBookings[date] = [];
+      }
+      groupedBookings[date].push(booking);
+      return groupedBookings;
+  }, {});
 }
 
 const BookingsComponent = () => {
-    const groupedBookings = groupByDay(mockCCData.data);
-
-    return (
-        <table className="w-full table-auto">
-            {Object.entries(groupedBookings).map(([date, bookings]: any, index) => (
-                <>
-                    <tr>
-                        <td colSpan={4} className="text-center bg-[#dfebeb] dark:bg-[#356c8e] p-2">
-                            {index === 0 ? 'Today' : date}
-                        </td>
-                    </tr>
-                    {bookings.map((booking:any) => (
-                        <tr>
-                            <td className="px-2"><a title={booking.name} data-tooltip-placement="top">{booking.callsign}</a></td>
-                            <td className="px-2">{bookingType(booking)}</td>
-                            <td className="px-2">{convertZulu(booking.time_start)}</td>
-                            <td className="px-2">{convertZulu(booking.time_end)}</td>
-                        </tr>
-                    ))}
-                </>
-            ))}
-        </table>
-    );
+  const groupedBookings = groupByDay(feed);
+  return (
+    <table className="w-full table-auto">
+      {Object.entries(groupedBookings).map(([date, bookings]: any, index) => (<>
+        <tr>
+          <td colSpan={4} className="text-center bg-[#dfebeb] dark:bg-[#356c8e] p-2">
+              {index === 0 ? 'Today' : date}
+          </td>
+        </tr>
+        {bookings.map((booking: any) => (<>
+        <tr className="[&:nth-child(even)]:bg-gray-200 dark:[&:nth-child(even)]:bg-[#1e3744]">
+          <td className="px-2">
+          {isOnline(booking.callsign) ? <span className="px-2">🟢</span> : <span className="px-2">🔴</span>}{booking.callsign}
+          </td>
+          <td className="px-2">{bookingType(booking)}</td>
+          <td className="px-2">{convertZulu(booking.time_start)}</td>
+          <td className="px-2">{convertZulu(booking.time_end)}</td>
+        </tr>
+      </>))}
+      </>
+    ))}
+    </table>
+  );
 };
 
 export default BookingsComponent;
