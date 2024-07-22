ea6e2a357ebe5ced51225813ca7b2f71bd94bdf0
Simon Bjerre <simon.bjerre@team.blue>
2024-07-22 22:37:50 +0200

    Added nice icons to the booking type

--- src/utils/bookingType.tsx
+++ src/utils/bookingType.tsx
@@ -1,10 +1,10 @@
 function bookingType(booking: any) {
     if (booking.training === 1) {
-        return <span className="bg-[#1a475f] text-sm text-white px-2 py-[2px] rounded-md">Training</span>
+        return <span className="bg-[#1a475f] text-sm text-white px-2 py-[2px] rounded-md flex items-center justify-center"><img src="/img/icons/traning.svg" className="w-auto h-4 pr-1" />Training</span>
     } else if (booking.event === 1) {
-        return <span className="bg-[#41826e] text-sm text-white px-2 py-[2px] rounded-md">Event</span>
+        return <span className="bg-[#41826e] text-sm text-white px-2 py-[2px] rounded-md flex items-center justify-center"><img src="/img/icons/event.svg" className="w-auto h-4 pr-1" />Event</span>
     } else if (booking.exam === 1) {
-        return <span className="bg-[#b63f3f] text-sm text-white px-2 py-[2px] rounded-md">Exam</span>
+        return <span className="bg-[#b63f3f] text-sm text-white px-2 py-[2px] rounded-md flex items-center justify-center"><img src="/img/icons/ctp.svg" className="w-auto h-4 pr-1" />Exam</span>
     } else {
         return ""
     }
