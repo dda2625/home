9dfee5e81ed1ac6f158c71c16942fd5bc8eb8c8e
Simon Bjerre <simon.bjerre@team.blue>
2024-07-23 21:57:44 +0200

    simplified the booking badge

--- src/utils/bookingType.tsx
+++ src/utils/bookingType.tsx
@@ -1,4 +1,57 @@
+import { Button } from "../components/ui/button"
+import {
+  Tooltip,
+  TooltipContent,
+  TooltipProvider,
+  TooltipTrigger,
+} from "../components/ui/tooltip"
+
 function bookingType(booking: any) {
+    if (booking.training === 1) {
+        return (
+        <TooltipProvider>
+            <Tooltip>
+              <TooltipTrigger asChild>
+                <span className="bg-[#1a475f] text-sm text-white px-2 py-[2px] rounded-md flex items-center justify-center"><img src="/img/icons/traning.svg" className="w-auto h-4 pr-1" /></span>
+              </TooltipTrigger>
+              <TooltipContent>
+                <p>Training</p>
+              </TooltipContent>
+            </Tooltip>
+        </TooltipProvider>
+        )
+    } else if (booking.event === 1) {
+        return (
+            <TooltipProvider>
+            <Tooltip>
+              <TooltipTrigger asChild>
+              <span className="bg-[#41826e] text-sm text-white px-2 py-[2px] rounded-md flex items-center justify-center"><img src="/img/icons/event.svg" className="w-auto h-4 pr-1" /></span>
+              </TooltipTrigger>
+              <TooltipContent>
+                <p>Event</p>
+              </TooltipContent>
+            </Tooltip>
+        </TooltipProvider>
+        )
+    } else if (booking.exam === 1) {
+        return (
+            <TooltipProvider>
+            <Tooltip>
+              <TooltipTrigger asChild>
+              <span className="bg-[#b63f3f] text-sm text-white px-2 py-[2px] rounded-md flex items-center justify-center"><img src="/img/icons/ctp.svg" className="w-auto h-4 pr-1" /></span>
+              </TooltipTrigger>
+              <TooltipContent>
+                <p>Exam</p>
+              </TooltipContent>
+            </Tooltip>
+        </TooltipProvider>
+        )
+    } else {
+        return ""
+    }
+}
+
+function bookingType_old(booking: any) {
     if (booking.training === 1) {
         return <span className="bg-[#1a475f] text-sm text-white px-2 py-[2px] rounded-md flex items-center justify-center"><img src="/img/icons/traning.svg" className="w-auto h-4 pr-1" />Training</span>
     } else if (booking.event === 1) {
