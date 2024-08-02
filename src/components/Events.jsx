2c396106a50da708d351940121748f4bb2671ae2
Simon Bjerre <bjerrecs@protonmail.com>
2024-08-03 00:12:25 +0200

    initial fetch from event service

--- src/components/Events.jsx
+++ src/components/Events.jsx
@@ -6,30 +6,78 @@ const Events = () => {
     useEffect(() => {
         const fetchData = async () => {
             try {
-                const response = await fetch('http://events.vatsim-scandinavia.org/api/calendars/1/events', {
+                const response = await fetch('https://events.vatsim-scandinavia.org/api/calendars/1/events', {
                     headers: {
-                        Authorization: 'Bearer 83e0745f-486d-4f73-8184-40c98930380c'
-                    },
-                    mode: 'no-cors' // Add the 'no-cors' mode
+                        Authorization: 'Bearer 83e0745f-486d-4f73-8184-40c98930380c' // Replace with your valid bearer token
+                    }
                 });
                 const data = await response.json();
-                setEvents(data);
+                setEvents(data.data);
             } catch (error) {
                 console.error('Error fetching events:', error);
             }
         };
-
+    
         fetchData();
     }, []);
 
+    function dateConverter(date) {
+        const newDate = new Date(date);
+        return newDate.toLocaleString('en-uk', {  weekday: 'short', month: 'short', day: 'numeric', hour: 'numeric', minute: 'numeric' });
+    }
+
+    console.log(dateConverter("2024-08-05 17:00:00"))
+    
     return (
-        <div>
-            <h1>Events</h1>
-            <ul>
-                {events.map(event => (
-                    <li key={event.id}>{event.title}</li>
-                ))}
-            </ul>
+        <div class="flex flex-col gap-4 md:w-full md:min-w-[900px] h-fit">
+            <div className="grid grid-cols-5 grid-rows-3 gap-4">
+                <div className="col-span-5 flex flex-row gap-4"> 
+                    <img src={events.length != 0 ? events[0].image  : ""}  className='w-96 h-fit aspect-video'/>
+                    <span>
+                        <h2 className='font-bold text-2xl'>{events.length != 0 ? events[0].title  : ""}</h2>
+                        <p>{events.length != 0 ? dateConverter(events[0].start_date)  : ""} - {events.length != 0 ? dateConverter(events[0].end_date)  : ""}</p>
+                        <p className='line-clamp-5'>{events.length != 0 ? events[0].long_description  : ""}</p>
+                    </span>
+                </div>
+                <div className="col-span-5 row-start-2 flex flex-row gap-4">
+                <img src={events.length != 0 ? events[1].image  : ""}  className='w-96 h-fit aspect-video'/>
+                    <span>
+                        <h2 className='font-bold text-2xl'>{events.length != 0 ? events[1].title  : ""}</h2>
+                        <p>{events.length != 0 ? dateConverter(events[1].start_date)  : ""} - {events.length != 0 ? dateConverter(events[1].end_date)  : ""}</p>
+                        <p className='line-clamp-5'>{events.length != 0 ? events[1].long_description  : ""}</p>
+                    </span>
+                </div>
+                <div className='col-span-5 row-start-3 flex flex-row gap-4 w-ful overflow-x-visible md:min-w-[900px] overflow-hidden'>
+                    <div className="flex flex-row gap-4 w-fit">
+                        <img src={events.length != 0 ? events[2].image  : ""}  className='w-48 h-fit aspect-video'/>
+                        <span>
+                            <h2 className='font-bold text-lg'>{events.length != 0 ? events[2].title  : ""}</h2>
+                            <p>{events.length != 0 ? dateConverter(events[2].start_date)  : ""} - {events.length != 0 ? dateConverter(events[2].end_date)  : ""}</p>
+                        </span>
+                    </div>
+                    <div className="flex flex-row gap-4">
+                        <img src={events.length != 0 ? events[3].image  : ""}  className='w-48 h-fit aspect-video'/>
+                        <span>
+                            <h2 className='font-bold text-lg'>{events.length != 0 ? events[3].title  : ""}</h2>
+                            <p>{events.length != 0 ? dateConverter(events[3].start_date)  : ""} - {events.length != 0 ? dateConverter(events[3].end_date)  : ""}</p>
+                        </span>
+                    </div>
+                    <div className="flex flex-row gap-4">
+                        <img src={events.length != 0 ? events[4].image  : ""}  className='w-48 h-fit aspect-video'/>
+                        <span>
+                            <h2 className='font-bold text-lg'>{events.length != 0 ? events[4].title  : ""}</h2>
+                            <p>{events.length != 0 ? dateConverter(events[4].start_date)  : ""} - {events.length != 0 ? dateConverter(events[4].end_date)  : ""}</p>
+                        </span>
+                    </div>
+                    <div className="flex flex-row gap-4">
+                        <img src={events.length != 0 ? events[5].image  : ""}  className='w-48 h-fit aspect-video'/>
+                        <span>
+                            <h2 className='font-bold text-lg'>{events.length != 0 ? events[5].title  : ""}</h2>
+                            <p>{events.length != 0 ? dateConverter(events[5].start_date)  : ""} - {events.length != 0 ? dateConverter(events[5].end_date)  : ""}</p>
+                        </span>
+                    </div>
+                </div>
+            </div>
         </div>
     );
 };
